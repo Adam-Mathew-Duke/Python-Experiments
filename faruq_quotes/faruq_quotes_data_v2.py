e044f9e806c55b4c0dab7653b69d9d35e025f9ba
Adam Duke <0soy.fish.studios0@gmail.com>
2024-09-05 23:23:26 +1000

    Create faruq_quotes_data_v2.py

---
+++ faruq_quotes/faruq_quotes_data_v2.py
@@ -0,0 +1,16 @@
+'''
+Used with faruq_quotes_v2.py
+'''
+
+faruq_quotes_list = [
+"Hi. It's Faruq, your friendly weatherman here, and I'd just like to say get inside yo damn house! here comes a <mark style=background:white>Monsoon!</mark>",
+"NASA may be heading to Mars, but this robot’s headed straight for Uranus!",
+"It'll leave you crying fowl and stick you with the bill. Release the quack-en! It's <mark style=background:yellow>Duck!</mark>",
+"From Seoul, South Korea, the movie Blade is rated 7.1 on IMDB, but this version is a perfect 10. Give it up for BLADE!",
+"From Nevada, the bot with hometown advantage. He'll outclass you in every way. He's got swag you can't manage. He's the king, you're the impersonator. He's blackjack, you're keno. He's the Bellagio, you're Circus Circus. He's Vegas, you're Reno. It's Jackpot!",
+"This brutal Bostonian machine is frostier than a Viking queen, and when you're home wondering what went wrong, you'll always hear its little song. Dun dun-dun-dun-dun-dun-dun! Valkyrie!",
+"Think your in Jeopardy, Oh no it's time to play wheel of misfortune, it's Nightmare!",
+"Every breath you take, every move you make he'll be wrecking you, it's Stinger",
+"He's so mean his opponents scream, please don't blaze me bro!, It's Saw Blaze",
+"It's robot fighting time!"
+]
