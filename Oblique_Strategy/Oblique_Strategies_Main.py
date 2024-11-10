75be1bce58a41b98b6b37df4c3342d42902c9e79
Adam Duke <0soy.fish.studios0@gmail.com>
2024-11-10 15:10:14 +1100

    Update Oblique_Strategies_Main.py

--- Oblique_Strategy/Oblique_Strategies_Main.py
+++ Oblique_Strategy/Oblique_Strategies_Main.py
@@ -1,4 +1,4 @@
 import streamlit, random
 from Oblique_Strategies_Data import quotes_list
-streamlit.subheader(random.choice(quotes_list))
+streamlit.write(random.choice(quotes_list))
 streamlit.button("New Oblique Strategy")
