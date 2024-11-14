4975af876b7deacce24c5ad152173e7cbe4fd76f
Adam Duke <0soy.fish.studios0@gmail.com>
2024-11-14 21:21:24 +1100

    Update Oblique_Strategies_Main.py

--- Oblique_Strategy/Oblique_Strategies_Main.py
+++ Oblique_Strategy/Oblique_Strategies_Main.py
@@ -1,12 +1,4 @@
 import streamlit, random
 from Oblique_Strategies_Data import quotes_list
-
-streamlit.html('<p style=font-family:Arial,sans-serif;font-size:25px;background-color:;text-align:center;padding-right:10px;padding-left:10px;padding-top:0px;padding-bottom:0px;>'+'Brian Eno’s Oblique Strategies (1975-1979)'+'</p>')
-streamlit.html('<p style=font-family:Arial,sans-serif;font-size:25px;background-color:black;text-align:center;padding-right:10px;padding-left:10px;padding-top:75px;padding-bottom:75px;>'+random.choice(quotes_list)+'</p>')
-
-left, middle, right = streamlit.columns(3)
-middle.button("New Oblique Strategy",use_container_width=True)
-
-# original code with no special formatting
-#streamlit.subheader(random.choice(quotes_list))
-#streamlit.button("New Oblique Strategy")
+streamlit.subheader(random.choice(quotes_list))
+streamlit.button("New Oblique Strategy")
